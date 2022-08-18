a7cd3fa28be9d1e25a382866aca5c4850e2ebcdd
pjaribadalu <boyepeju@gmail.com>
2022-08-17 22:06:17 -0400

    Adding iam file

---
+++ iam.tf
@@ -0,0 +1,2 @@
+# What am i doing with this file. 
+# I am certain that God will make a way for me despite the eay it looks. I am going places and doors are opening for me. My trust is in God and not man. I will not be deterred by the demons that are trying to frustrate me.
